89486148b9bd391b207e04ffc89a633fe6a07e51
Carolyne Golias <goliascarolyne@gmail.com>
2025-07-27 14:31:21 -0700

    basic layout for nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+
 export function NavBar() {
-  return null;
+  return (
+    <>
+      <div className="flex w-full bg-gray-500 h-16 items-center justify-end px-10 space-x-4">
+        <Link href="/">Home</Link>
+        <Link href="/explore">Explore</Link>
+        <Link href="/about-us">About us</Link>
+        <FontAwesomeIcon icon={faUser} className="h-6 w-6 text-white" />
+        {/* {isLoggedIn && (
+          <FontAwesomeIcon icon={faHeart} className="h-6 w-6 text-white ml-4" />
+        )} */}
+      </div>
+    </>
+  );
 }
