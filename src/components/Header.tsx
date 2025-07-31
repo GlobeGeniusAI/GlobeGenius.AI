1586880db759e27986ce032b9b29caa58603c1a8
Carolyne Golias <goliascarolyne@gmail.com>
2025-07-31 10:58:52 -0700

    rename header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,41 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+import Image from "next/image";
 export function Header() {
-  return null;
+  return (
+    <>
+      <nav className="flex w-full bg-zinc-600 h-16 items-center justify-end px-10 space-x-4">
+        <Image
+          src="/Logo.png"
+          width={150}
+          height={40}
+          alt="Globe Genius Logo"
+          className="text-center"
+        />
+        <Link href="/" className="text-white mr-8 font-bold hover:underline">
+          Home
+        </Link>
+        <Link
+          href="/explore"
+          className="text-white mr-8 font-bold hover:underline"
+        >
+          Explore
+        </Link>
+        <Link
+          href="/about-us"
+          className="text-white mr-12 font-bold hover:underline"
+        >
+          About us
+        </Link>
+        <FontAwesomeIcon
+          icon={faUser}
+          className="h-6 w-6 text-white font-bold"
+        />
+        {/* {isLoggedIn && (
+          <FontAwesomeIcon icon={faHeart} className="h-6 w-6 text-white ml-4" />
+        )} */}
+      </nav>
+    </>
+  );
 }
