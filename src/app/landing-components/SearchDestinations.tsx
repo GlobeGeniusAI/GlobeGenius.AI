ad01d81f7e8df16bb00a3677506526f9f7616c24
Ally Greven <allysongreven@gmail.com>
2025-08-04 19:51:38 -0700

    structure of landing page plus one

--- src/app/landing-components/SearchDestinations.tsx
+++ src/app/landing-components/SearchDestinations.tsx
@@ -1,6 +1,6 @@
 export default function SearchDestinations() {
     return (
-        <div className="font-montserrat text-[#000744] font-semibold text-3xl">
+        <div className="font-montserrat text-[#000744] font-semibold text-3xl ">
             <h1>Search Destinations</h1>
         </div>
     )
